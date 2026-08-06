6445ab098024adfe1fa47001dd7854f836e34d99
Mikenume <minunezme@gmail.com>
2026-08-06 14:36:47 +0200

    Esqueleto inicial: Spring Boot + JPA + PostgreSQL

---
+++ src/test/java/com/miguel/games_collection/GamesCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.games_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
